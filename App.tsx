b6c8618690b54afcba1066c53783818d7035d782
Basar <basarsubasi@basarsubasi.com.tr>
2024-12-06 13:20:01 +0300

    asdf

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working oPOOOOOn your app!</Text>
+      <Text>Open up App.tsx to start working dsankdnas your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
