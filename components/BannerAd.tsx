ec804157b46cdd601ebfc7b44822aae47b0ebb9b
basar <basarsubasi@protonmail.com>
2024-12-19 18:16:09 +0300

    removed unused import

--- components/BannerAd.tsx
+++ components/BannerAd.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+import { BannerAd, BannerAdSize} from 'react-native-google-mobile-ads';
 import { useAdContext } from '../context/AdContext';
 
 interface BannerAdProps {
